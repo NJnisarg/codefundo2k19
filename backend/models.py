72f2995356bf27d2243cedb4a61d3ab84fe70856
yashmagarwal786 <yashmagarwal786@gmail.com>
2019-08-13 00:36:02 +0530

    Database Schema defined in Models

--- backend/models.py
+++ backend/models.py
@@ -3,15 +3,40 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Election(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    start_date = models.DateField()
+    end_date = models.DateField()
 
-class AadharData(models.Model):
+class Constituency(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
 
-	id = models.IntegerField(primary_key=True),
-	aid = models.CharField(unique=True, null=False, blank=False, max_length=128),
-	cid = models.IntegerField(null=True, blank=True),
-	fingerPrint = models.CharField(unique=True, null=False, blank=False, max_length=256),
-	mobile_num = models.CharField(unique=True, null=False, blank=False, max_length=15)
-class Authentication(models.Model):
-    personId = models.TextField()
-    mobNo = models.TextField()
-    fingerSignature = models.TextField()#finger print
+class AadharDetail(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    mobile_num = models.CharField(max_length=20)
+    aadhar_num = models.CharField(max_length=20)
+    finger_print = models.TextField(max_length=255)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=20)
+    address = models.TextField(max_length=255)
+    pincode = models.CharField(max_length=20)
+    constituency_id = models.ForeignKey(Constituency, on_delete=models.CASCADE)
+
+class ElectionConstituency(models.Model):
+    id = models.IntegerField(primary_key=True)
+    election_id = models.ForeignKey(Election, on_delete=models.CASCADE)
+    constituency_id = models.ForeignKey(Constituency, on_delete=models.CASCADE)
+
+class Party(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+
+class PartyCandidate(models.Model):
+    id = models.IntegerField(primary_key=True)
+    party_id = models.ForeignKey(Party, on_delete=models.CASCADE)
+    aadhar_detail_id = models.ForeignKey(AadharDetail, on_delete=models.CASCADE)
+    election_id = models.ForeignKey(Election, on_delete=models.CASCADE)
+    constituency_id = models.ForeignKey(Constituency, on_delete=models.CASCADE)
--- backend/urls.py
+++ backend/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import AuthAPI, AadharDataAPI
+from .views import AuthAPI
 app_name = "backend"
 
 
 urlpatterns = [
-     path('backend/auth/', AuthAPI.as_view()),
-     path('backend/aadharData/', AadharDataAPI.as_view()),
-]    
+     path('auth/', AuthAPI.as_view()),
+]
