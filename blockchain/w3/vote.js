91a576ea306187bd83cdcf9d24744a1f7d05f62e
njnisarg <NJnisarg@gmail.com>
2019-08-18 02:42:33 +0530

    Completed seeding of data and adding candidates to blockchain

--- blockchain/w3/vote.js
+++ blockchain/w3/vote.js
@@ -1,73 +1,68 @@
 const Web3 = require('web3');
 const VotingContractArtifact = require('../build/contracts/Voting.json');
 
-const networkAddr = "http://ethdnxd24-dns-reg1.eastus.cloudapp.azure.com:8540";
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const rpc_endpoint = 'http://ethdnxd24-dns-reg1.eastus.cloudapp.azure.com:8540';
+const fs = require('fs');
+const mnemonic = fs.readFileSync("../metamask_wallet.txt").toString().trim();
+
 const contractABI = VotingContractArtifact["abi"];
 const contractAddress = VotingContractArtifact["networks"]["10101010"]["address"];
 
-const web3 = new Web3();
-web3.setProvider(new web3.providers.HttpProvider(networkAddr));
-
+const web3 = new Web3(new HDWalletProvider(mnemonic, rpc_endpoint));
 const contract = new web3.eth.Contract(contractABI, contractAddress);
 
+const defaultSender = '0x38D4010731de6dee73F15aF71c886AB0e49f5122';
 
 const vote = async (candidateHash, voterHash) => {
-    const accounts = await web3.eth.getAccounts();
-    const defaultSender = accounts[0];
 
-    contract.methods.vote(candidateHash, voterHash).send({from: defaultSender})
-        .then((result) => {
-            console.log(result);
-            return true;
-        })
-        .catch((error) => {
-            console.log(error);
-            return false;
-        });
+    try{
+        return await contract.methods.vote(candidateHash, voterHash).send({from: defaultSender});
+    } catch(err) {
+        return null;
+    }
 };
 
 const getVoteCount = async (candidateHash) => {
-
-    contract.methods.getVoteCount(candidateHash).call()
-        .then(async (result) => {
-            console.log(await result);
-            return result;
-        })
-        .catch((error) => {
-            console.log(error);
-            return null;
-        });
+    try{
+        return await contract.methods.getVoteCount(candidateHash).call();
+    }catch(err) {
+        return null;
+    }
 };
 
 const getVoters = async () => {
 
-    contract.methods.getVoters().call()
-        .then(async (result) => {
-            console.log(await result);
-            return result;
-        })
-        .catch((error) => {
-            console.log(error);
-            return null;
-        });
+    try {
+        return await contract.methods.getVoters().call();
+    }catch(err){
+        return null;
+    }
+
 };
 
 const getCandidates = async () => {
 
-    contract.methods.getCandidates().call()
-        .then((result) => {
-            console.log(result);
-            return result;
-        })
-        .catch((error) => {
-            console.log(error);
-            return null;
-        });
+    try{
+        return await contract.methods.getCandidates().call();
+    }catch(err) {
+        return null;
+    }
+};
+
+const reset = async () => {
+
+    try{
+        return await contract.methods.reset().send({from: defaultSender})
+    }catch(err) {
+        return null;
+    }
 };
 
 module.exports = {
     vote,
     getVoteCount,
     getVoters,
-    getCandidates
+    getCandidates,
+    reset
 };
