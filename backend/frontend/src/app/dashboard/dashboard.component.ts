cf6d5d1e38efb30dfa987cd645f5159a814ac0ba
mananpoddar <mananpoddarm@gmail.com>
2019-08-19 21:38:40 +0530

    add w3 service

--- backend/frontend/src/app/dashboard/dashboard.component.ts
+++ backend/frontend/src/app/dashboard/dashboard.component.ts
@@ -4,7 +4,7 @@ import { AadharService  } from '../_service/aadhar.service';
 import { stringify } from '@angular/compiler/src/util';
 import { NgxUiLoaderService } from 'ngx-ui-loader';
 import {vote} from '../../../../../blockchain/w3/addEntities';
-
+import { W3Service } from '../_service/w3.service';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -14,13 +14,32 @@ import {vote} from '../../../../../blockchain/w3/addEntities';
 export class DashboardComponent implements OnInit {
   public aadhar:any;
   public userId;
-  constructor(private route: ActivatedRoute,public aadharService:AadharService,private ngxLoader: NgxUiLoaderService) { }
+  public userKey;
+  public userAadharNum;
+  public randNum;
+  constructor(private route: ActivatedRoute,public aadharService:AadharService,private ngxLoader: NgxUiLoaderService,public w3Service:W3Service) { }
+
+  public addVoter(userKey){
+      this.w3Service.addVoter(userKey).subscribe(
+        res=>{
+          console.log(res);
+        }
+      )
+  }
 
   public getAadharDetails(){
     this.aadharService.getAadharDetails(this.userId).subscribe(
       res=>{
         this.aadhar = res;
         console.log(this.aadhar);
+        this.userAadharNum = this.aadhar.aadhar_num;
+        let rand = Math.floor(Math.random() * 10000000000000000);
+        this.randNum = rand.toString();
+        localStorage.setItem("randNum",this.randNum);
+        this.userKey = this.userAadharNum.concat(rand);
+        console.log("manan")
+        console.log(this.userKey);
+        this.addVoter(this.userKey);
       }
     )
   }
--- blockchain/server.js
+++ blockchain/server.js
@@ -4,8 +4,18 @@ const {vote, getVoteCount,getCandidates, getVoters} = require('./w3/vote');
 const express = require('express');
 const app = express();
 const port = 3000;
+const cors = require('cors');
+
+
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
+  
 
 app.use(express.json());
+app.use(cors())
 
 app.post('/addVoter',async (req, res) => {
     let voterHash = req.body.voterHash;
