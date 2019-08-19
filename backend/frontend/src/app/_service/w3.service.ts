5a77e6a289aceb82b8a9cdc7bf69488dd40a003c
mananpoddar <mananpoddarm@gmail.com>
2019-08-20 01:52:59 +0530

    add service and UI for result component

--- backend/frontend/src/app/_service/w3.service.ts
+++ backend/frontend/src/app/_service/w3.service.ts
@@ -41,4 +41,22 @@ export class W3Service {
     return res;
   }
 
+  public getVoteCount(userId,electionId){
+    this.data={
+      "user_id":userId,
+      "election_id":electionId
+    }
+    console.log(this.data);
+
+    this._url = `${this.apiUrl}/getVoteCount/`;
+    console.log(this._url);
+    
+    var res = this.http.post(
+      this._url,
+      this.data
+    );
+    
+    return res;
+  }
+
 }
--- backend/frontend/src/app/results/results.component.ts
+++ backend/frontend/src/app/results/results.component.ts
@@ -1,6 +1,10 @@
 declare var require: any
 
 import { Component, OnInit } from '@angular/core';
+import { W3Service } from '../_service/w3.service';
+import { environment } from '../../environments/environment'
+
+const _ = require('underscore');
 const Candidates = require('../../assets/images.json');
 const c = Candidates.projects;
 const candidates = Candidates.categories;
@@ -11,10 +15,32 @@ const candidates = Candidates.categories;
 })
 export class ResultsComponent implements OnInit {
   categories = candidates;
-  constructor() { }
+  public electionId;
+  public userId;
+  public resultData;
+  public env = environment;
+  public domain = this.env['apiUrl'];
+  constructor(public w3Service:W3Service) { }
+
+  public getVoteCount(){
+    console.log("here ")
+    this.w3Service.getVoteCount(this.userId,this.electionId).subscribe(
+      res=>{
+        console.log("res")
+        console.log(res);
+        this.resultData = res;
+        this.resultData = _.sortBy(this.resultData, 'vote_count').reverse();
+        console.log(this.resultData);
+      }
+    )
+  }
+  
 
   ngOnInit() {
     console.log(this.categories);
+    this.electionId = localStorage.getItem("electionId");
+    this.userId = localStorage.getItem("userId");
+    this.getVoteCount();
 
   }
 
