6dde3ecf4d4f6f417a64057c819c9ac407dac5be
mananpoddar <mananpoddarm@gmail.com>
2019-08-19 12:18:14 +0530

    add styling for elections component

--- backend/frontend/src/app/elections/elections.component.ts
+++ backend/frontend/src/app/elections/elections.component.ts
@@ -2,7 +2,33 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { ElectionsDetailService } from '../_service/elections-detail.service';
 import { ActivatedRoute, Router } from '@angular/router';
 
-
+var RTIdata = [
+  {
+    title:" Education ",
+    desc:" description of what all is under this tag",
+    link:" link to rti tag"
+  },
+  {
+    title:" Health ",
+    desc:" description of what all is under this tag",
+    link:" link to rti tag"
+  },
+  {
+    title:" Roads ",
+    desc:" description of what all is under this tag",
+    link:" link to rti tag"
+  },
+  {
+    title:" Criminal charges ",
+    desc:" description of what all is under this tag",
+    link:" link to rti tag"
+  },
+  {
+    title:" Salary ",
+    desc:" description of what all is under this tag",
+    link:" link to rti tag"
+  }
+]
 
 @Component({
   selector: 'app-elections',
@@ -14,6 +40,7 @@ export class ElectionsComponent implements OnInit {
   public elections;
   public userId;
   public upcomingOrpast;
+  public rtiData;
   constructor(public electionsService:ElectionsDetailService,private route: ActivatedRoute,public router:Router) { }
 
   public getUpcomingElections(){
@@ -55,6 +82,7 @@ export class ElectionsComponent implements OnInit {
       this.getPastElections();
     }
     
+    this.rtiData = RTIdata;
     
   }
 
